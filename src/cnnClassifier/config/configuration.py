e5f3ffafbb8caab43109973d01e71eab4bb92d40
Akash <akashkulkarni4444@gmail.com>
2024-04-11 15:35:47 +0530

    model evaluation and dvc

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,7 @@
 from cnnClassifier.constants import *
 import os
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig
+from cnnClassifier.entity.config_entity import DataIngestionConfig, EvaluationConfig, PrepareBaseModelConfig, TrainingConfig
 class ConfigurationManager:
     def __init__(
         self,
